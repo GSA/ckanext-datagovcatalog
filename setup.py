fb9a281cf9f654c4cb00110f715497fd27220128
Jin-Sun-tts <jin.sun@gsa.gov>
2022-07-11 14:44:30 -0400

    lint: removed whitespace

--- setup.py
+++ setup.py
@@ -63,7 +63,7 @@ setup(
         # http://docs.ckan.org/en/latest/extensions/best-practices.html#add-third-party-libraries-to-requirements-txt
     ],
     setup_requires=['wheel'],
-    
+
     # If there are data files included in your packages that need to be
     # installed, specify them here.  If using Python 2.6 or less, then these
     # have to be included in MANIFEST.in as well.
